e547d6b86001edde28c3b1f8933320798e05f09f
stevennn <1136671955@qq.com>
2019-04-21 18:30:27 +0800

    初始化

---
+++ pythonBase/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+        司马懿：“善败能忍，然厚积薄发”
+                                    ——李叔说的
+code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+          --┃      ☃      ┃--
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗II━II┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+ @Belong = 'StevennnTips'  @MadeBy = 'PyCharm'
+ @Author = 'steven'   @DateTime = '2019/4/21 15:39'
+'''
